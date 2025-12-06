de47a88068b8d508b5bb5332c8497f0189f8cff3
Damnstolar <damnstolar@MacBook-Air-Damnstolar.local>
2025-12-06 22:43:45 +0100

    .

--- girlprojekt/main.cc
+++ girlprojekt/main.cc
@@ -94,7 +94,6 @@ int main(int argc, char *argv[]) {
   defaults.cols = 128;
   defaults.chain_length = 1;
   defaults.parallel = 1;
-  defaults.gpio_slowdown = 3;
   rgb_matrix::RuntimeOptions runtime_opt;
   if (!rgb_matrix::ParseOptionsFromFlags(&argc, &argv, &defaults, &runtime_opt)) {
     return 1;
